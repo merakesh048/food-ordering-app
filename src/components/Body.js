b655b3eb781cf127bdb1bb697b0c328349b0ad61
merakesh048 <rksirts@gmail.com>
2026-01-10 02:28:55 +0530

    fetched date from live API

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,8 +1,68 @@
 import RestaurantCard from "./RestaurantCard";
-import resList from "../utils/mockData";
-import { useState } from "react";
+//import resList from "../utils/mockData";
+import { useEffect, useState } from "react";
 const Body = () => {
-  const [listOfRestaurant, setListOfRestaurant] = useState(resList);
+  const [listOfRestaurant, setListOfRestaurant] = useState([]);
+
+  useEffect(() => {
+    //API Call
+    fetchData();
+  }, []);
+
+  // const fetchData = async () => {
+  //   try {
+  //     const data = await fetch(
+  //       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
+  //     );
+
+  //     const json = await data.json();
+
+  //     setListOfRestaurant(
+  //       json?.data?.cards[1]?.card?.gridElements?.infoWithStyle?.restaurants
+  //     );
+
+  //     console.log(
+  //       json?.data?.cards[1]?.card?.gridElements?.infoWithStyle?.restaurants
+  //     );
+  //   } catch (error) {
+  //     console.error("Error fetching data:", error);
+  //   }
+  // };
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json = await data.json();
+    console.log(json);
+
+    // json?.data?.cards?.forEach((card, index) => {
+    //   const restaurants =
+    //     card?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+
+    //   if (restaurants) {
+    //     console.log("✅ Restaurants found at index:", index);
+    //     console.log("🍽️ Restaurants data:", restaurants);
+    //   }
+    // });
+    setListOfRestaurant(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants || []
+    );
+
+    // const restaurantCard = json?.data?.cards?.find(
+    //   (card) => card?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    // );
+
+    // const restaurants =
+    //   restaurantCard?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+
+    // // console.log(restaurants);
+
+    // setListOfRestaurant(restaurants || []);
+  };
+
   return (
     <div className="body">
       <div className="filter">
